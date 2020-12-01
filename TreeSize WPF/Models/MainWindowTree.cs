a127615737848528305252b868197065c8d6acc5
Alex <www.emko.ru@gmail.com>
2020-12-01 22:52:24 +0200

    AddProject from another repo

---
+++ TreeSize WPF/Models/MainWindowTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeSize_WPF.Models
+{
+    public class MainWindowTree
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public long FreeSpace { get; set; }
+        public int Files { get; set; }
+        public int Folders { get; set; }
+        public DateTime LastModified { get; set; }
+
+        public ObservableCollection<MainWindowTree> SubTrees { get; set; }
+    }
+}
